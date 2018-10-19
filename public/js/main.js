4972075b186eff574a36273ef748f87ac63526c3
Victor Mann <victorcmann@gmail.com>
2018-10-19 14:36:16 -0300

    criando modulo principal main.js

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+// módulo principal
+/**
+ *  Args: primeiro é o nome do módulo que desejamos criar e o 
+ *  segundo é uma array com todos os módulos de que nosso 
+ *  módulo depende
+ */
+angular.module('alurapic', []);
