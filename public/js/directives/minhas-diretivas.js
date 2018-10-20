061c234b6c6e1ebd9e42406b61745b112b912512
Victor Mann <victorcmann@gmail.com>
2018-10-20 11:20:18 -0300

    criando modulo minhasDiretivas

---
+++ public/js/directives/minhas-diretivas.js
@@ -0,0 +1,8 @@
+angular.module('minhasDiretivas', [])
+// o nome da diretiva deve ser camel case
+.directive('meuPainel', function () {
+    // directive definition object (DDO)
+    var ddo = {};
+
+    return ddo;
+});
