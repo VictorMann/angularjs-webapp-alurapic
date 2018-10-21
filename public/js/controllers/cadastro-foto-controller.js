7384bd39c8bb2b5b9d26a0c81e051ad3494233c2
Victor Mann <victorcmann@gmail.com>
2018-10-21 14:03:54 -0200

    definindo validação ao enviar cadastro

--- public/js/controllers/cadastro-foto-controller.js
+++ public/js/controllers/cadastro-foto-controller.js
@@ -1,8 +1,21 @@
-angular.module('alurapic').controller('CadastroFotoController', function ($scope) {
+angular.module('alurapic').controller('CadastroFotoController', function ($scope, $http) {
 
     $scope.foto = {};
+    $scope.mensagem = '';
 
     $scope.submeter = function () {
-        console.log($scope.foto)
+        
+        if ($scope.formulario.$valid) {
+
+            $http.post('v1/fotos', $scope.foto)
+            .success(function () {
+                $scope.foto = {};
+                $scope.mensagem = 'Foto incluida com sucesso';
+            })
+            .error(function (erro) {
+                $scope.mensagem = 'Não foi possível incluir';
+                console.log(erro);
+            })
+        }
     };
 });
