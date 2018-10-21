f20910e30c5cbc04587b26c80ccb80ed0759f6a8
Victor Mann <victorcmann@gmail.com>
2018-10-21 11:24:31 -0200

    configurando ngRoute no main.js sistema de rotas para fotos

--- public/js/main.js
+++ public/js/main.js
@@ -5,4 +5,16 @@
  *  módulo depende
  */
 // angular.module('alurapic', ['minhasDiretivas']);
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+.config(function ($routeProvider) {
+    // define rota para lista de fotos
+    $routeProvider.when('/fotos', {
+        templateUrl: 'partials/principal.html',
+        controller: 'FotosController'
+    });
+
+    // redireciona caso não encontre uma rota
+    $routeProvider.otherwise({
+        redirectTo: '/fotos'
+    });
+});
