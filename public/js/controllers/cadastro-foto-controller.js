c5a2f0f654fd7d0933ad2595d265ea9fef640fd8
Victor Mann <victorcmann@gmail.com>
2018-10-22 21:24:25 -0200

    modificando CadastroFotoController para manipular edicao da foto

--- public/js/controllers/cadastro-foto-controller.js
+++ public/js/controllers/cadastro-foto-controller.js
@@ -1,21 +1,46 @@
-angular.module('alurapic').controller('CadastroFotoController', function ($scope, $http) {
+angular.module('alurapic').controller('CadastroFotoController', function ($scope, $http, $routeParams) {
 
     $scope.foto = {};
     $scope.mensagem = '';
 
+    // caso para edição
+    var id = $routeParams.fotoId;
+    if (id) {
+        $http.get('v1/fotos/' + id)
+        .success(function (foto) {
+            $scope.foto = foto;
+        })
+        .error(function (erro) {
+            $scope.mensagem = 'Foto não encontrada';
+        });
+    }
+
     $scope.submeter = function () {
         
         if ($scope.formulario.$valid) {
+            // PUT : edição
+            if (id) {
+                $http.put('v1/fotos/' + id, $scope.foto)
+                .success(function () {
+                    $scope.mensagem = 'Foto atualizada!';
+                })
+                .error(function (erro) {
+                    $scope.mensagem = 'Erro ao atualizar a foto';
+                    console.log(erro);
+                });
 
-            $http.post('v1/fotos', $scope.foto)
-            .success(function () {
-                $scope.foto = {};
-                $scope.mensagem = 'Foto incluida com sucesso';
-            })
-            .error(function (erro) {
-                $scope.mensagem = 'Não foi possível incluir';
-                console.log(erro);
-            })
+            // POST : novo
+            } else {
+                $http.post('v1/fotos', $scope.foto)
+                .success(function () {
+                    $scope.foto = {};
+                    $scope.mensagem = 'Foto incluida com sucesso';
+                })
+                .error(function (erro) {
+                    $scope.mensagem = 'Não foi possível incluir';
+                    console.log(erro);
+                })
+            }
         }
     };
 });
